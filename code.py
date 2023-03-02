6c9d9113869b621e3ae0ff4662fc8ab7568e8232
Mohamed Hamisa <108889205+MohamedHamisa@users.noreply.github.com>
2023-03-02 21:56:06 +0200

    Create code.py

---
+++ code.py
@@ -0,0 +1,4 @@
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        return root if (root.val - p.val) * (root.val - q.val) < 1 else \
+            self.lowestCommonAncestor((root.left, root.right)[p.val > root.val], p, q)
